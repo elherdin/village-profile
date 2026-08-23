48029e1cae3900fd37c5b98067a452a670ac607f
HerdinsyahAnhar <elherdin@gmail.com>
2026-08-23 16:37:52 +0700

    chore: update production environment API and backend URLs and remove system metadata files

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // Ganti dengan Domain Backend Coolify Anda (misal: https://api.plantungan.desa.id/api)
-  apiUrl: "https://api.plantungan.desa.id/api",
-  backendUrl: "https://api.plantungan.desa.id"
+  apiUrl: "https://api-desaplantungan.albyta.com/api",
+  backendUrl: "https://api-desaplantungan.albyta.com"
 };
